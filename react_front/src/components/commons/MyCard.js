27a5f528c773cca68b5944e81becd53867f26428
jaybon1 <ponggas2@gmail.com>
2022-12-17 17:40:59 +0900

    카드 완성

--- react_front/src/components/commons/MyCard.js
+++ react_front/src/components/commons/MyCard.js
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Card, Col, Image, InputGroup, Row } from "react-bootstrap";
 import NoimageImg from "assets/img/no-image.png";
 import LikeImg from "assets/img/like.svg";
 
 const MyCard = ({ post }) => {
+  /** @type {React.CSSProperties} cardContainer */
+  const cardContainer = useMemo(() => {
+    return {
+      height: "150px",
+      overflow: "hidden",
+    };
+  }, []);
+
+  /** @type {React.CSSProperties} cardImg */
+  const cardImg = useMemo(() => {
+    return {
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+      objectPosition: "center",
+    };
+  }, []);
+
+  /** @type {React.CSSProperties} cardText */
+  const cardText = useMemo(() => {
+    return {
+      display: "-webkit-box",
+      wordWrap: "break-word",
+      WebkitLineClamp: 4,
+      WebkitBoxOrient: "vertical",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      height: "100px",
+    };
+  }, []);
+
+  /** @type {React.CSSProperties} cardTitle */
+  const cardTitle = useMemo(() => {
+    return {
+      cursor: "pointer",
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    };
+  }, []);
+
   return (
     <Col>
       <Card className="m-3">
-        <Card.Img
-          variant="top"
-          src={post.thumbnail ? post.thumbnail : NoimageImg}
-        />
+        <div style={cardContainer}>
+          <Card.Img
+            variant="top"
+            src={post.thumbnail ? post.thumbnail : NoimageImg}
+            style={cardImg}
+          />
+        </div>
         <Card.Body>
-          <Card.Title>{post.title}</Card.Title>
-          <Card.Text>{post.summary}</Card.Text>
-          <small className="text-muted mx-3 mb-2">{post.createDate}</small>
+          <Card.Title style={cardTitle}>{post.title}</Card.Title>
+          <Card.Text style={cardText}>{post.summary}</Card.Text>
+          <small className="text-muted">{post.createDate}</small>
         </Card.Body>
         <Card.Footer>
           <Row>
@@ -29,7 +73,7 @@ const MyCard = ({ post }) => {
                 <strong>{post.writer.id}</strong>
               </InputGroup>
             </Col>
-            <Col>
+            <Col className="col-auto">
               <InputGroup>
                 <Image src={LikeImg} width="15" />
                 <span className="mx-2 fs-6 text-black-50 fw-light">
